aee9a1e2b6a365cfc7b89ea32de07524b0435887
Taaylored <tprince09@eagles.ewu.edu>
2016-06-07 15:51:43 -0700

    adjusted type and colors

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,21 @@
-// Animate this wheel
+
 var $colorwheel = $('#colorwheel');
 
-
-// Dom Element, duration, properties to change
 TweenMax.to($colorwheel, 4, {
 
-    rotation: 1440,
-//   repeat: 2,
-// ease: Power1.easeInOut
-    
-    
+    rotation: 1440,    
 });
 
 
-TweenLite.from('text', 3, { 
+TweenLite.from('#text', 3, { 
   delay: 3,
   opacity: 0,
   
   });
 
 
-
-// Small wheels
-
-// Animate this wheel
 var $smallwheel = $('.smallwheel');
 
-
-// Dom Element, duration, properties to change
 TweenMax.to($smallwheel, 6, {
 
     rotation: 1220,
