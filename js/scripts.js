18a7973ce822bc6075aa24055e70d40d30c2ed40
Taaylored <tprince09@eagles.ewu.edu>
2016-06-07 12:10:08 -0700

    design added

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,53 @@
-// Animate this freakin star boi
-var $star = $('#star');
+// Animate this wheel
+var $colorwheel = $('#colorwheel');
 
 
 // Dom Element, duration, properties to change
-TweenMax.to($star, 1, {
+TweenMax.to($colorwheel, 4, {
 
-    opacity: 0,
+    rotation: 1440,
+//   repeat: 2,
+// ease: Power1.easeInOut
+    
     
 });
 
+
+TweenLite.from('#v', 3, { 
+  delay: 1,
+    left: 300,
+  opacity: 0
+  });
+
+TweenLite.from('#c', 3, { 
+  delay: 2,
+    bottom: 300,
+  opacity: 0
+  });
+
+TweenLite.from('#d', 3, { 
+  delay: 3,
+    right: 300,
+  opacity: 0
+  });
+
+TweenLite.from('#day', 3, { 
+  delay: 4,
+  opacity: 0
+  });
+
+// Small wheels
+
+// Animate this wheel
+var $smallwheel = $('.smallwheel');
+
+
+// Dom Element, duration, properties to change
+TweenMax.to($smallwheel, 4, {
+
+    rotation: 1440,
+   repeat: -1,
+ ease: Power1.easeInOut
+    
+    
+});
