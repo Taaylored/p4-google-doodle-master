d20ddc93a1e58f9cb71a9a9f0889b641a5cda9a9
Taaylored <tprince09@eagles.ewu.edu>
2016-06-09 12:28:56 -0700

    added new animations

--- js/scripts.js
+++ js/scripts.js
@@ -8,22 +8,11 @@ TweenMax.to($colorwheel, 4, {
 
 
 TweenLite.from('#text', 2.5, { 
-  delay: 2,
+  delay: 1,
   ease: Circ.easeIn, y: 0,
   opacity: 0,
   
   });
 
-
-var $smallwheel = $('.smallwheel');
-
-TweenMax.to($smallwheel, 6, {
-
-    rotation: 1220,
-   repeat: -1,
- 
-    
-    
-});
 
 
